dddaea8c77bac9f7fb259eba71585164a4456761
Justin Merz <jrmerz@gmail.com>
2016-03-04 15:12:02 -0800

    first pass at command line tool

---
+++ cmd/index.js
@@ -0,0 +1,29 @@
+var program = require('commander');
+var crawler = require('../lib/crawler');
+
+program
+  .version('0.0.1')
+  .option('-p, --parse-data', 'Parse data files (csv, json, etc)')
+  .option('-r, --reindex', 'Walk data directory and generate root network.json & regions.json files before running')
+  .option('-v, --verbose', 'Verbose')
+  .option('-d, --directory [directory]', 'Root data directory to crawl')
+  .parse(process.argv);
+
+if( !program.directory ) {
+  console.error('Directory Required');
+  process.exit(-1);
+}
+
+var options = {
+  parseCsvData: program.parseData || false,
+  debug: program.verbose || false,
+  reindex : program.reindex || false
+};
+
+
+crawler(program.directory, options, function(resp){
+  resp.nodes.features.forEach(function(node){
+   console.log(node.properties);
+  //  console.log();
+  });
+});
