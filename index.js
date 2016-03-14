7b9c64bdf3ec0ea3ea80d24872edbd7f8df7f0c8
Justin Merz <jrmerz@gmail.com>
2016-03-14 14:54:17 -0700

    quick fix

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var crawler = require('./lib/crawler');
 
-module.exports = function(o) {
+module.exports = function(path, o, callback) {
+  if( typeof o === 'function' ) {
+    callback = o;
+    o = {};
+  }
+
   var options = {
     parseCsvData: o.parseCsvData || false,
     debug: o.verbose || false,
