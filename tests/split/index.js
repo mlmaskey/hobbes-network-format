398fc6fdfac987f95db72f360dc87b0c2ac5e98e
Justin Merz <jrmerz@gmail.com>
2016-04-20 12:14:57 -0700

    merge node/links for edges on split

--- tests/split/index.js
+++ tests/split/index.js
@@ -20,10 +20,8 @@ describe('hnf.split', function() {
     hnf.split(config.path, {}, data.nodes, function(resp){
 
       assert.deepEqual([ 'SR_WHI','D5','SR_WHI-D5' ], resp.in.map(toIds));
-      assert.deepEqual([ 'SR_SHA', 'D94' ], resp.edge.in.nodes.map(toIds));
-      assert.deepEqual([ 'SR_SHA-D5', 'D94-SR_WHI' ], resp.edge.in.links.map(toIds));
-      assert.deepEqual([ 'HSU101SR3', 'D73', 'WTP101', 'HSU101D5' ], resp.edge.out.nodes.map(toIds));
-      assert.deepEqual([ 'SR_WHI-HSU101SR3', 'SR_WHI-D73', 'D5-WTP101', 'D5-HSU101D5', 'D5-D73' ], resp.edge.out.links.map(toIds));
+      assert.deepEqual([ 'SR_SHA', 'D94' , 'SR_SHA-D5', 'D94-SR_WHI' ], resp.edge.in.map(toIds));
+      assert.deepEqual([ 'HSU101SR3', 'D73', 'WTP101', 'HSU101D5','SR_WHI-HSU101SR3', 'SR_WHI-D73', 'D5-WTP101', 'D5-HSU101D5', 'D5-D73' ], resp.edge.out.map(toIds));
 
       next();
     });
